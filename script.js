dc793227862ddea2df1bc1488ccaaa2f1b71cfe9
LoDestino <151969703+LoDestino@users.noreply.github.com>
2024-02-14 21:35:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+function Login(){
+    var user = document.getElementById("entry1").value;
+    var password = document.getElementById("entry2").value;
 
+    if (user === "Destiny" && password === "1234"){
+        alert("User: Destiny has successfully logged in.")
+    }
+}
